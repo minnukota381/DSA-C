479c238daf9e3ff2eefa0a054ecbe9953d4ce328
minnukota381 <minnukota381@gmail.com>
2025-01-05 12:00:00 +0530

    Graph BFS Traversal using Adjacency List in C

---
+++ Graphs/BFSAdjacencyList.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int vertex;
+    struct Node* next;
+};
+
+struct Graph {
+    int V;
+    struct Node** adjList;
+};
+
+struct Node* createNode(int vertex) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->vertex = vertex;
+    newNode->next = NULL;
+    return newNode;
+}
+
+struct Graph* createGraph(int V) {
+    struct Graph* graph = (struct Graph*)malloc(sizeof(struct Graph));
+    graph->V = V;
+    graph->adjList = (struct Node**)malloc(V * sizeof(struct Node*));
+
+    for (int i = 0; i < V; i++) {
+        graph->adjList[i] = NULL;
+    }
+
+    return graph;
+}
+
+void addEdge(struct Graph* graph, int u, int v) {
+    struct Node* newNode = createNode(v);
+    newNode->next = graph->adjList[u];
+    graph->adjList[u] = newNode;
+
+    newNode = createNode(u);
+    newNode->next = graph->adjList[v];
+    graph->adjList[v] = newNode;
+}
+
+void BFS(struct Graph* graph, int startVertex) {
+    int visited[graph->V];
+    for (int i = 0; i < graph->V; i++) {
+        visited[i] = 0;
+    }
+
+    int queue[graph->V];
+    int front = -1, rear = -1;
+
+    visited[startVertex] = 1;
+    queue[++rear] = startVertex;
+
+    while (front != rear) {
+        int currentVertex = queue[++front];
+        printf("%d -> ", currentVertex);
+
+        struct Node* temp = graph->adjList[currentVertex];
+        while (temp) {
+            if (!visited[temp->vertex]) {
+                visited[temp->vertex] = 1;
+                queue[++rear] = temp->vertex;
+            }
+            temp = temp->next;
+        }
+    }
+}
+
+int main() {
+    struct Graph* graph = createGraph(4);
+    addEdge(graph, 0, 1);
+    addEdge(graph, 0, 2);
+    addEdge(graph, 1, 3);
+
+    printf("BFS Traversal starting from vertex 0:\n");
+    BFS(graph, 0);
+
+    return 0;
+}
